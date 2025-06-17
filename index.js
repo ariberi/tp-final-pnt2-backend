5be848a305d8dd972c457bb7db2d6f599dadb85d
Ariel <berinsteinariel@gmail.com>
2025-06-16 21:50:04 -0300

    .

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/api', router);
+
 await connection.sync({force:false})
 
 app.listen(SERVER_PORT, () => {
--- services/AuthService.js
+++ services/AuthService.js
@@ -3,20 +3,22 @@ import { User } from '../models/index.js';
 import { JWT_SECRET } from '../config/config.js';
 
 class AuthService {
+
     async register(data) {
         const user = await User.create(data);
         const token = this.generateToken(user.id);
         return { user, token };
     }
 
-    async login({ email, pass }) {
+    async login({ email, password }) {
         const user = await User.findOne({ where: { email } });
         if (!user) throw new Error('User not found');
-        const valid = await user.validPassword(pass);
+        const valid = await user.validPassword(password);
         if (!valid) throw new Error('Invalid credentials');
         const token = this.generateToken(user.id);
         return { user, token };
     }
+
 
     generateToken(userId) {
         return jwt.sign({ userId }, JWT_SECRET, { expiresIn: '7d' });
