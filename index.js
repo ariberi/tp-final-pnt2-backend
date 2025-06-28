1e9c86781b2685b6debc77ba62187c7521031d50
Ariel <berinsteinariel@gmail.com>
2025-06-28 13:42:23 -0300

    add cors

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ import connection from './connection/connection.js';
 import router from './router/router.js';
 import cookieParser from 'cookie-parser';
 import { SERVER_PORT } from './config/config.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
