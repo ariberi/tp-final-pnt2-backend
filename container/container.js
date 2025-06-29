655441974e737b864f6b8541802b42bd8effe39e
camila <camilacore1230170@gmail.com>
2025-06-29 18:46:23 -0300

    conectar authController por inyección en container y router

--- container/container.js
+++ container/container.js
@@ -1,9 +1,18 @@
 import UserService from '../services/UserService.js';
 import UserController from '../controllers/UserController.js';
 
+import AuthService from '../services/AuthService.js';
+import AuthController from '../controllers/AuthController.js'
+
+// INYECCION DE DEPENDENCIA USERS
 const userService = new UserService();
 const userController = UserController(userService);
 
+// INYECCION DE DEPENDENCIAS AUTH
+const authService = new AuthService();
+const authController = AuthController(authService);
+
 export default {
-  userController
+  userController,
+  authController
 };
--- router/authRouter.js
+++ router/authRouter.js
@@ -1,13 +1,19 @@
 import { Router } from 'express';
-import { register, login, logout, me} from '../controllers/AuthController.js';
+// import { register, login, logout, me} from '../controllers/AuthController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { authController } from '../container/container'
+
+
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', logout);
-router.get('/me',authMiddleware, me);
-
+// router.post('/register', register);
+// router.post('/login', login);
+// router.post('/logout', logout);
+// router.get('/me',authMiddleware, me);
+router.post('./login', authController.login);
+router.post('./register', authController.register);
+router.post('/logout', authController.logout);
+router.get('/me', authMiddleware, authController.me); 
 
 export default router;
